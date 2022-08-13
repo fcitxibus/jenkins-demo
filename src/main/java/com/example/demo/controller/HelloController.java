a126f4d640b3b65d3d9af3206a7be316adac6503
catkillfish <catkillfish@163.com>
2022-08-13 11:13:26 +0800

    第三次提交

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,13 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @Autowired
-    StringRedisTemplate redisTemplate;
-
-    @GetMapping("/hello")
+//    @GetMapping("/hello")
+    @RequestMapping("hello")
     public String hello(){
-        Long views = redisTemplate.opsForValue().increment("views");
-        return "hello,My linux,haha,views:" + views;
+        return  "hello";
     }
 
 }
